f4df272f3ae7d18baf35bb310c2e835f6e6794ee
Vitor Sabatin <vitor.okubo@hotmail.com>
2024-11-18 03:16:52 -0300

    DI

---
+++ ClientManager.Infrastructure.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,33 @@
+﻿using ClientManager.Application.Interfaces;
+using ClientManager.Application.Mappings;
+using ClientManager.Application.Services;
+using ClientManager.Domain.Interfaces;
+using ClientManager.Infrastructure.Context;
+using ClientManager.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ClientManager.Infrastructure.IoC
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options => 
+            options.UseNpgsql(configuration.GetConnectionString("WebApiDatabase"), b => 
+            b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IClienteRepository, ClienteRepository>();
+
+            services.AddScoped<IProdutoService, ProdutoService>();
+            services.AddScoped<IClienteService, ClienteService>();
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));   
+
+
+            return services;
+        }
+    }
+}
