aa231a9667199b0d43d6482b714efedb31d39d89
Vitor Sabatin <vitor.okubo@hotmail.com>
2024-11-18 09:35:39 -0300

    Repositorio Venda e ProdutoVenda

---
+++ ClientManager.Infrastructure/Repositories/ProdutoVendaRepository.cs
@@ -0,0 +1,48 @@
+﻿using ClientManager.Domain.Entities;
+using ClientManager.Domain.Interfaces;
+using ClientManager.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManager.Infrastructure.Repositories
+{
+    public class ProdutoVendaRepository : IProdutoVendaRepository
+    {
+
+        AppDbContext _produtoVendaContext;
+
+        public ProdutoVendaRepository(AppDbContext context)
+        {
+            _produtoVendaContext = context;
+        }
+
+        public Task<ProdutoVenda> CreateAsync(ProdutoVenda produtoVenda)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProdutoVenda> GetByIdAsync(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProdutoVenda>> GetProdutoVendasAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProdutoVenda> RemoveAsync(ProdutoVenda produtoVenda)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProdutoVenda> UpdateAsync(ProdutoVenda produtoVenda)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ClientManager.Infrastructure/Repositories/VendaRepository.cs
@@ -0,0 +1,59 @@
+﻿using ClientManager.Domain.Entities;
+using ClientManager.Domain.Interfaces;
+using ClientManager.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManager.Infrastructure.Repositories
+{
+    public class VendaRepository : IVendaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public VendaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Venda> CreateAsync(Venda venda)
+        {
+            _context.Add(venda);
+            await _context.SaveChangesAsync();
+            return venda;
+        }
+
+        public async Task<Venda?> GetByIdAsync(int? id)
+        {
+            return await _context.Vendas
+                                 .Include(v => v.Vendas) 
+                                 .ThenInclude(pv => pv.Produto) 
+                                 .FirstOrDefaultAsync(v => v.Id == id);
+        }
+
+        public async Task<IEnumerable<Venda>> GetVendasAsync()
+        {
+            return await _context.Vendas
+                                 .Include(v => v.Vendas) 
+                                 .ThenInclude(pv => pv.Produto) 
+                                 .ToListAsync();
+        }
+
+        public async Task<Venda> RemoveAsync(Venda venda)
+        {
+            _context.Remove(venda);
+            await _context.SaveChangesAsync();
+            return venda;
+        }
+
+        public async Task<Venda> UpdateAsync(Venda venda)
+        {
+            _context.Update(venda);
+            await _context.SaveChangesAsync();
+            return venda;
+        }
+    }
+}
