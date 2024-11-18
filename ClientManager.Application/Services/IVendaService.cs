67bf4544de868fe0e177758db8b86cae1ae8f9e7
Vitor Sabatin <vitor.okubo@hotmail.com>
2024-11-18 09:42:45 -0300

    VendaService

---
+++ ClientManager.Application/Services/IVendaService.cs
@@ -0,0 +1,13 @@
+﻿using ClientManager.Application.DTOs;
+
+namespace ClientManager.Application.Services
+{
+    public interface IVendaService
+    {
+        Task Add(VendaDTO vendaDTO);
+        Task<VendaDTO> GetById(int? id);
+        Task<List<VendaDTO>> GetVendas();
+        Task Remove(int? id);
+        Task Update(VendaDTO vendaDTO);
+    }
+}
---
+++ ClientManager.Application/Services/VendaService.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using ClientManager.Application.DTOs;
+using ClientManager.Application.Interfaces;
+using ClientManager.Domain.Entities;
+using ClientManager.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManager.Application.Services
+{
+    public class VendaService : IVendaService
+    {
+        private readonly IVendaRepository _vendaRepository;
+        private readonly IMapper _mapper;
+
+        public VendaService(IVendaRepository vendaRepository, IMapper mapper)
+        {
+            _vendaRepository = vendaRepository;
+            _mapper = mapper;
+        }
+
+        public async Task Add(VendaDTO vendaDTO)
+        {
+            var vendaEntity = _mapper.Map<Venda>(vendaDTO);
+            await _vendaRepository.CreateAsync(vendaEntity);
+        }
+
+        public async Task<VendaDTO> GetById(int? id)
+        {
+            var vendaEntity = await _vendaRepository.GetByIdAsync(id);
+            return _mapper.Map<VendaDTO>(vendaEntity);
+        }
+
+        public async Task<List<VendaDTO>> GetVendas()
+        {
+            var vendasEntity = await _vendaRepository.GetVendasAsync();
+            return _mapper.Map<List<VendaDTO>>(vendasEntity);
+        }
+
+        public async Task Remove(int? id)
+        {
+            var vendaEntity = await _vendaRepository.GetByIdAsync(id);
+            if (vendaEntity != null)
+            {
+                await _vendaRepository.RemoveAsync(vendaEntity);
+            }
+        }
+
+        public async Task Update(VendaDTO vendaDTO)
+        {
+            var vendaEntity = _mapper.Map<Venda>(vendaDTO);
+            await _vendaRepository.UpdateAsync(vendaEntity);
+        }
+    }
+
+}
