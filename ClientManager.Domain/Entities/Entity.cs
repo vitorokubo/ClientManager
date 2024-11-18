dfc02d8f8d61acf1da79288e3d0b4d504cd8d789
Vitor Sabatin <vitor.okubo@hotmail.com>
2024-11-18 03:12:24 -0300

    Criação Entity Model e ProdutoVenda Model

---
+++ ClientManager.Domain/Entities/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManager.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public int Id { get; protected set; } 
+        // DateTime CreatedDate {get;set;}
+        // string CreatedBy { get; set; }
+    }
+}
---
+++ ClientManager.Domain/Entities/ProdutoVenda.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManager.Domain.Entities
+{
+    public sealed class ProdutoVenda: Entity
+    {
+        public int QuatidadeVendida { get; private set; }
+        public double PrecoPorUnidade { get; private set; }
+        public int ProdutoId { get; private set; }
+        public Produto Produto { get; private set; }
+        public int VendaId { get; private set; }
+        public Produto Venda { get; private set; }
+
+        public double PrecoTotal => QuatidadeVendida * PrecoPorUnidade;
+
+    }
+}
