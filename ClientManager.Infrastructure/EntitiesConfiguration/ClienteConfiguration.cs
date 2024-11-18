29b4230e0252064b15f36e05c036d2d736931413
Vitor Sabatin <vitor.okubo@hotmail.com>
2024-11-18 03:14:34 -0300

    Cliente/Produto Configuration Db

---
+++ ClientManager.Infrastructure/EntitiesConfiguration/ClienteConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using ClientManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManager.Infrastructure.EntitiesConfiguration
+{
+    internal class ClienteConfiguration : IEntityTypeConfiguration<Cliente>
+    {
+        public void Configure(EntityTypeBuilder<Cliente> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x=> x.Name).HasMaxLength(100).IsRequired();
+        }
+    }
+}
---
+++ ClientManager.Infrastructure/EntitiesConfiguration/ProdutoConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using ClientManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManager.Infrastructure.EntitiesConfiguration
+{
+    internal class ProdutoConfiguration : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x=> x.Name).HasMaxLength(100).IsRequired();
+        }
+    }
+}
