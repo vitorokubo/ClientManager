7f9db80a10fdcb4673c86218b9e300d50a0e804d
Vitor Sabatin <vitor.okubo@hotmail.com>
2024-11-18 03:16:28 -0300

    Repositorio Produto/Cliente

---
+++ ClientManager.Infrastructure/Repositories/ClienteRepository.cs
@@ -0,0 +1,54 @@
+﻿using ClientManager.Domain.Entities;
+using ClientManager.Domain.Interfaces;
+using ClientManager.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManager.Infrastructure.Repositories
+{
+    public class ClienteRepository : IClienteRepository
+    {
+
+        AppDbContext _clienteContext;
+
+        public ClienteRepository(AppDbContext context)
+        {
+            _clienteContext = context;
+        }
+
+        public async Task<Cliente> CreateAsync(Cliente Cliente)
+        {
+            _clienteContext.Add(Cliente);
+            await _clienteContext.SaveChangesAsync();
+            return Cliente;
+        }
+
+        public async Task<Cliente?> GetByIdAsync(int? id)
+        {
+            return await _clienteContext.Clientes.FindAsync(id);
+        }
+
+        public async Task<List<Cliente>> GetClientesAsync()
+        {
+            return await _clienteContext.Clientes.ToListAsync();
+        }
+
+        public async Task<Cliente> RemoveAsync(Cliente Cliente)
+        {
+            _clienteContext.Remove(Cliente);
+            await _clienteContext.SaveChangesAsync();
+            return Cliente;
+        }
+
+        public async Task<Cliente> UpdateAsync(Cliente Cliente)
+        {
+            _clienteContext.Update(Cliente);
+            await _clienteContext.SaveChangesAsync();
+            return Cliente;
+        }
+    }
+}
---
+++ ClientManager.Infrastructure/Repositories/ProdutoRepository.cs
@@ -0,0 +1,54 @@
+﻿using ClientManager.Domain.Entities;
+using ClientManager.Domain.Interfaces;
+using ClientManager.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManager.Infrastructure.Repositories
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+
+        AppDbContext _produtoContext;
+
+        public ProdutoRepository(AppDbContext context)
+        {
+            _produtoContext = context;
+        }
+
+        public async Task<Produto> CreateAsync(Produto produto)
+        {
+            _produtoContext.Add(produto);
+            await _produtoContext.SaveChangesAsync();
+            return produto;
+        }
+
+        public async Task<Produto?> GetByIdAsync(int? id)
+        {
+            return await _produtoContext.Produtos.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Produto>> GetProdutosAsync()
+        {
+            return await _produtoContext.Produtos.ToListAsync();
+        }
+
+        public async Task<Produto> RemoveAsync(Produto produto)
+        {
+            _produtoContext.Remove(produto);
+            await _produtoContext.SaveChangesAsync();
+            return produto;
+        }
+
+        public async Task<Produto> UpdateAsync(Produto produto)
+        {
+            _produtoContext.Update(produto);
+            await _produtoContext.SaveChangesAsync();
+            return produto;
+        }
+    }
+}
