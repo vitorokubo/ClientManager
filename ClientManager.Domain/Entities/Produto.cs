1386013455439d24b48369c30322253c85553e32
Vitor Sabatin <vitor.okubo@hotmail.com>
2024-11-18 03:11:52 -0300

    Criação Produto Model

---
+++ ClientManager.Domain/Entities/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManager.Domain.Entities
+{
+    public sealed class Produto : Entity
+    {
+        public string Name { get; private set; }
+        public string Descricao { get; private set; }
+        public int Estoque {  get; private set; }
+
+    }
+}
