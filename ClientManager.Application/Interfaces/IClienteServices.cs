82adb7f3af16746747f72262dc8530ae342ba7df
Vitor Sabatin <vitor.okubo@hotmail.com>
2024-11-18 03:18:22 -0300

    Interfaces Cliente/Produto do Application

---
+++ ClientManager.Application/Interfaces/IClienteServices.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using ClientManager.Application.DTOs;
+using ClientManager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManager.Application.Interfaces
+{
+    public interface IClienteService
+    {
+        Task<List<ClienteDTO>> GetClientes();
+        Task<ClienteDTO> GetById(int? id);
+
+        Task Add(ClienteDTO clienteDTO);
+        Task Update(ClienteDTO clienteDTO);
+        Task Remove(int? id);
+    }
+}
---
+++ ClientManager.Application/Interfaces/IProdutoServices.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using ClientManager.Application.DTOs;
+using ClientManager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManager.Application.Interfaces
+{
+    public interface IProdutoService
+    {
+        Task<List<ProdutoDTO>> GetProdutos();
+        Task<ProdutoDTO> GetById(int? id);
+
+        Task Add(ProdutoDTO produtoDTO);
+        Task Update(ProdutoDTO produtoDTO);
+        Task Remove(int? id);
+    }
+}
