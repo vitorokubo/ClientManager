5fbea9b0bcd425a2400071b022169e4bb6b00397
Vitor Sabatin <vitor.okubo@hotmail.com>
2024-11-18 03:18:33 -0300

    Cliente e Produto Service

---
+++ ClientManager.Application/Services/ClienteService.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using ClientManager.Application.DTOs;
+using ClientManager.Application.Interfaces;
+using ClientManager.Domain.Entities;
+using ClientManager.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManager.Application.Services
+{
+    public class ClienteService : IClienteService
+    {
+
+        private IClienteRepository _clienteRepository;
+        private readonly IMapper _mapper;
+
+        public ClienteService(IClienteRepository clienteRepository, IMapper mapper)
+        {
+            _clienteRepository = clienteRepository; 
+            _mapper = mapper;
+        }
+
+        public async Task Add(ClienteDTO clienteDTO)
+        {
+            var clienteEntity = _mapper.Map<Cliente>(clienteDTO);
+            await _clienteRepository.CreateAsync(clienteEntity);
+        }
+
+        public async Task<ClienteDTO> GetById(int? id)
+        {
+            var clienteEntity = await _clienteRepository.GetByIdAsync(id);
+            return _mapper.Map<ClienteDTO>(clienteEntity);
+        }
+
+        public async Task<List<ClienteDTO>> GetClientes()
+        {
+            var clientesEntity = await _clienteRepository.GetClientesAsync();
+            return _mapper.Map<List<ClienteDTO>>(clientesEntity);
+        }
+
+        public async Task Remove(int? id)
+        {
+            var clienteEntity = _clienteRepository.GetByIdAsync(id).Result; 
+            if(clienteEntity != null) await _clienteRepository.RemoveAsync(clienteEntity);
+        }
+
+        public async Task Update(ClienteDTO clienteDTO)
+        {
+            var clienteEntity = _mapper.Map<Cliente>(clienteDTO);
+            await _clienteRepository.UpdateAsync(clienteEntity);
+        }
+
+    }
+}
---
+++ ClientManager.Application/Services/ProdutoService.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using ClientManager.Application.DTOs;
+using ClientManager.Application.Interfaces;
+using ClientManager.Domain.Entities;
+using ClientManager.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManager.Application.Services
+{
+    public class ProdutoService : IProdutoService
+    {
+
+        private IProdutoRepository _produtoRepository;
+        private readonly IMapper _mapper;
+
+        public ProdutoService(IProdutoRepository produtoRepository, IMapper mapper)
+        {
+            _produtoRepository = produtoRepository; 
+            _mapper = mapper;
+        }
+
+        public async Task Add(ProdutoDTO produtoDTO)
+        {
+            var produtoEntity = _mapper.Map<Produto>(produtoDTO);
+            await _produtoRepository.CreateAsync(produtoEntity);
+
+        }
+
+        public async Task<ProdutoDTO> GetById(int? id)
+        {
+            var produtoEntity = await _produtoRepository.GetByIdAsync(id);
+            return _mapper.Map<ProdutoDTO>(produtoEntity);
+        }
+
+        public async Task<List<ProdutoDTO>> GetProdutos()
+        {
+            var produtosEntity = await _produtoRepository.GetProdutosAsync();
+            return _mapper.Map<List<ProdutoDTO>>(produtosEntity);
+        }
+
+        public async Task Remove(int? id)
+        {
+            var produtoEntity = _produtoRepository.GetByIdAsync(id).Result; 
+            if(produtoEntity != null) await _produtoRepository.RemoveAsync(produtoEntity);
+        }
+
+        public async Task Update(ProdutoDTO produtoDTO)
+        {
+            var produtoEntity = _mapper.Map<Produto>(produtoDTO);
+            await _produtoRepository.UpdateAsync(produtoEntity);
+        }
+    }
+}
