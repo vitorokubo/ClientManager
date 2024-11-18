d81dbfeccf1357015eac114fca9d5402ee59279e
Vitor Sabatin <vitor.okubo@hotmail.com>
2024-11-18 03:14:11 -0300

    Criação DbContext e Factory

---
+++ ClientManager.Infrastructure/Context/AppContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ClientManager.Infrastructure.Context
+{
+    public class AppContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=meu_banco;Username=user;Password=123456");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+
+}
---
+++ ClientManager.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientManager.Domain.Entities;
+
+namespace ClientManager.Infrastructure.Context
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+
+    }
+
+
+}
