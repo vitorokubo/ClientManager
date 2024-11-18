7196e48b2342c10186c68125619585e503db91c5
Vitor Sabatin <vitor.okubo@hotmail.com>
2024-11-18 03:11:59 -0300

    Criação Venda Model

---
+++ ClientManager.Domain/Entities/Venda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManager.Domain.Entities
+{
+    public sealed class Venda : Entity
+    {
+
+        public int ClienteId { get; private set; }
+        public Cliente Cliente { get; private set; }
+        public List<ProdutoVenda> Vendas {  get; private set; }
+
+        public double VendaTotal => Vendas.Sum(x=> x.PrecoTotal);
+        
+
+    }
+}
