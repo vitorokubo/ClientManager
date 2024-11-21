f93341bd545c7f2f710f6f630363777e7ad47581
Vitor Sabatin <vitor.okubo@hotmail.com>
2024-11-21 08:09:03 -0300

    Migrations

--- ClientManager.Infrastructure/Migrations/20241121024415_Correction_QuantidadeProperty.Designer.cs
+++ ClientManager.Infrastructure/Migrations/20241121024415_Correction_QuantidadeProperty.Designer.cs
@@ -2,6 +2,7 @@
 using ClientManager.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ClientManager.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241121024415_Correction_QuantidadeProperty")]
+    partial class Correction_QuantidadeProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ClientManager.Infrastructure/Migrations/20241121024415_Correction_QuantidadeProperty.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClientManager.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Correction_QuantidadeProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuatidadeVendida",
+                table: "ProdutoVendas",
+                newName: "QuantidadeVendida");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantidadeVendida",
+                table: "ProdutoVendas",
+                newName: "QuatidadeVendida");
+        }
+    }
+}
