08bbb45c533be6e3642c70fc79dc5de2fe61367e
Vitor Sabatin <vitor.okubo@hotmail.com>
2024-11-21 08:09:28 -0300

    Update ProdutoVenda Entity

--- ClientManager.Domain/Entities/ProdutoVenda.cs
+++ ClientManager.Domain/Entities/ProdutoVenda.cs
@@ -8,14 +8,14 @@ namespace ClientManager.Domain.Entities
 {
     public sealed class ProdutoVenda: Entity
     {
-        public int QuatidadeVendida { get; private set; }
+        public int QuantidadeVendida { get; private set; }
         public double PrecoPorUnidade { get; private set; }
         public int ProdutoId { get; private set; }
         public Produto Produto { get; private set; }
         public int VendaId { get; private set; }
         public Venda Venda { get; private set; }
 
-        public double PrecoTotal => QuatidadeVendida * PrecoPorUnidade;
+        public double PrecoTotal => QuantidadeVendida * PrecoPorUnidade;
 
     }
 }
