0ddbb84b33d30f6ee27c200ca7706490a42fc8e9
Vitor Sabatin <vitor.okubo@hotmail.com>
2024-11-25 08:26:15 -0300

    AuthenticateService e Interfaces

---
+++ ClientManager.Infrastructure/Identity/AuthenticateService.cs
@@ -0,0 +1,57 @@
+﻿using ClientManager.Domain.Account;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManager.Infrastructure.Data.Identity
+{
+    public class AuthenticateService : IAuthenticate
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AuthenticateService(UserManager<ApplicationUser> userManager,SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public async Task<bool> AuthenticateAsync(string email, string password)
+        {
+
+            var user = await _userManager.FindByEmailAsync(email);
+
+            if (user == null)
+            {
+                return false;
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(user, password, false, false);
+
+            return result.Succeeded;
+        }
+
+        public async Task<bool> RegisterUser(string email, string password)
+        {
+            var applicationUser = new ApplicationUser()
+            {
+                UserName = email,
+                Email = password 
+            };   
+            var result = await _userManager.CreateAsync(applicationUser,password);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(applicationUser, isPersistent: false);
+            }
+            return result.Succeeded;
+        }
+
+        public Task Logout()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
