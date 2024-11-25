f17803d32cc0fe83b17a34cbab29e6cc81681386
Vitor Sabatin <vitor.okubo@hotmail.com>
2024-11-25 08:25:38 -0300

    Add ApplicationUserIdentity

--- ClientManager.Infrastructure/Context/AppDbContext.cs
+++ ClientManager.Infrastructure/Context/AppDbContext.cs
@@ -1,15 +1,12 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ClientManager.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using ClientManager.Infrastructure.Data.Identity;
 
 namespace ClientManager.Infrastructure.Context
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
